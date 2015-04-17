9cf92b5d3d1be28da8d7193d32199cf3513c0977
David Knoll <knolladavid@gmail.com>
2015-04-17 13:04:45 -0600

    final commit

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -1,97 +1,124 @@
-﻿
-
-﻿using System;
+﻿﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Threading;
-
 
 namespace HangNail
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            
-               bool keepPlaying = true;
-            
-        
+            string username = string.Empty;
+
+            Console.WriteLine(@"
+            (/)
+            (/)
+             (/)
+            (/)
+             (/)
+            (/)
+            (/))
+           (/)(/)
+          (/)'`(/)
+         (/)    (/)
+         (/)    (/)
+         (/)    (/)
+         (/)    (/)
+          (/)  (/)
+           (/)(/)
+            `""`");
+
+            Console.WriteLine(@"/\  /\ __ _  _ __    __ _   /\/\    __ _  _ __
+ / /_/ // _` || '_ \  / _` | /    \  / _` || '_ \
+/ __  /| (_| || | | || (_| |/ /\/\ \| (_| || | | |
+\/ /_/  \__,_||_| |_| \__, |\/    \/ \__,_||_| |_|");
+
             Console.WriteLine("Ladies and Gentlmen, step right up and play the game thats sweeping the nation.  Why hello kindly villager, what be your name? ");
+            username = Console.ReadLine();
+
+            Console.WriteLine("Welcome " + username + ". We are going to be play ye' olde game of hangman, but with a slight twist.\n This is called hangnail. With every letter guessed incorrectly,\n the skin of the cuticle is ripped further down.\n This is a game that will take skill and cunning to make it through unscathed.\nYou will have to guess the word, but be careful, you only have a 8 lives.\n Guess too many wrong answers and you'll lose your LIFE! Can you guess the word?\n  ");
             Console.ReadLine();
-            Console.WriteLine("We are going to be play ye' olde game of hangman, but with a slight twist.\n This is called hangnail. With every letter guessed incorrectly,\n the skin of the cuticle is ripped further down.\n This is a game that will take skill and cunning to make it through unscathed.\nYou will have to guess the word, but be careful, you only have a 8 lives.\n Guess too many wrong answers and you'll lose Can you guess the word?\n  ");
-            Console.ReadLine();
+
+            Console.Clear();
             Console.WriteLine("Press any key to continue...");
             Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine(@"
+            (/)
+            (/)
+             (/)
+            (/)
+             (/)
+            (/)
+            (/))
+           (/)(/)
+          (/)'`(/)
+         (/)    (/)
+         (/)    (/)
+         (/)    (/)
+         (/)    (/)
+          (/)  (/)
+           (/)(/)
+            `""`");
 
+            Console.WriteLine(@"/\  /\ __ _  _ __    __ _   /\/\    __ _  _ __
+ / /_/ // _` || '_ \  / _` | /    \  / _` || '_ \
+/ __  /| (_| || | | || (_| |/ /\/\ \| (_| || | | |
+\/ /_/  \__,_||_| |_| \__, |\/    \/ \__,_||_| |_|");
+            Console.WriteLine("Great to hear " + username + ", lets play ");
 
-           Console.WriteLine("Great to hear, lets play ");
             HangNail();
 
             Console.ReadKey(); //keep the console open
         }
-        
-    
-   
-            
-        
-        
-        
-        
-        static void HangNail()
-        {
-            //boolean to hold whether to continue the game.
-            bool playing = true;
-            string playerName; //hold the player's name
-            int lives =8 ; //number of guesses
-            
 
+        private static void HangNail()
+        {
+            //boolean value to tell console whether to continue to play
+            bool playing = true;
+
+            // the amount of lives remaining in the counter
+            int lives = 8;
 
             //Create our word bank, obviously
-            List<string> wordBank = new List<string>() { "banana", "meatloaf", "rap", "rock" , "football"};
-            
+            List<string> wordStorage = new List<string>() { "banana", "meatloaf", "rap", "rock", "football" };
 
-            Random rng = new Random(); //make a new rng
-            //select a random number between 0 and the #
-            // of things in our wordBank List
-            int randomNumber = rng.Next(0, wordBank.Count());
-            //choose a random item from our wordBank List
+            Random rng = new Random();
+
+            int randomNumber = rng.Next(0, wordStorage.Count());
+            //choose a random word from the word bank or invisibility 
             // force it to be UPPERCASE
-            string wordToGuess = wordBank[randomNumber].ToUpper();
+            string wordToGuess = wordStorage[randomNumber].ToUpper();
 
-            //one line solution for getting a random item
-            //string wordToGuess = wordBank[new Random().Next(0, wordBank.Count())];
-
-            //need to track what letters they've guessed
+            
             string lettersGuessed = string.Empty;
 
-            //start our game loop
+            //Begin with our game loop 
             while (playing)
             {
-                //1. show the masked word
-                Console.WriteLine(MaskedWord(wordToGuess, lettersGuessed));
-                //2. tell them how many lives left
+                //this will show our hidden word
+                Console.WriteLine(Invisibility(wordToGuess, lettersGuessed));
+
+                //tell the user how many lives they have remaining
                 Console.WriteLine("Let me remdind you that you only have  " + lives + " lives remaining");
-                //3. ask for input
+                //let them begin the  game
                 Console.WriteLine("Guess away!");
-                //4. get the input, force it to be UPPERCASE
-                string input = Console.ReadLine().ToUpper();
 
-                //determine if its a letter or a word guess
-                if (input.Length == 1)
+                string userInput = Console.ReadLine().ToUpper();
+
+                //is it a letter or a full guessed word?
+                if (userInput.Length == 1)
                 {
-                    //guessed a letter, add it to the string
-                    lettersGuessed += input;
+                    //guessed a letter that is the word, add it to the string
+                    lettersGuessed += userInput;
 
-                    //letter guess.  determine if its a
-                    // correct guess
-                    if (wordToGuess.Contains(input))
+                    if (wordToGuess.Contains(userInput))
                     {
-                        //correct guess!
+                        //if they correctly guess the word
                         Console.WriteLine("Good Guess, Not too shabby!");
-                        //determine if they have guessed all the letters in the word
-                        if (AllLettersGuessed(MaskedWord(wordToGuess, lettersGuessed)))
+
+                        //has the user guessed all the letters in the word?
+                        if (AllLettersGuessed(Invisibility(wordToGuess, lettersGuessed)))
                         {
                             playing = false;
                             Console.WriteLine("Winner, Winner, Chicken Dinner!");
@@ -99,61 +126,52 @@ namespace HangNail
                     }
                     else
                     {
-                        //incorrect guess!
+                        //user has given an incorrect guess
                         Console.WriteLine("Ha ha ha.... Nope");
                         lives--;
                     }
                 }
                 else
                 {
-                   
-                  
-                    if (wordToGuess == input)
+                    if (wordToGuess == userInput)
                     {
-                        //they beat the game 
+                        //they beat the game . WAY TO GO!
                         Console.WriteLine("Sweet Baby Jesus, you're smart!");
                         playing = true;
                     }
                     else
                     {
-                        //lose a life
+                        //lost a Life
                         Console.WriteLine("Another one bites the dust");
                         lives--;
                     }
                 }
-                //check for a loss because of life
+                //check to see if game is over because of lives
                 if (lives == 0)
                 {
                     playing = false;
                     Console.WriteLine("Oh, too bad! I thought you had it. ");
                 }
-
             }
-
         }
 
-     
-        static string MaskedWord(string wordToGuess, string lettersGuessed)
+        private static string Invisibility(string wordToGuess, string lettersGuessed)
         {
-            
             string returnString = "";
 
-            //loop through to examine chars and look for letters 
+            //loop through to examine chars and look for letters
             for (int i = 0; i < wordToGuess.Length; i++)
             {
                 // use i index to get char from wordtoguess
                 char letter = wordToGuess[i];
 
-
                 //check to see if letter guessed is in wordtoguess
                 if (lettersGuessed.ToUpper().Contains(Char.ToUpper(letter)))
                 {
-                    
                     returnString += letter + " ";
                 }
                 else
                 {
-                    
                     returnString += "_" + " ";
                 }
             }
@@ -161,12 +179,11 @@ namespace HangNail
             return returnString;
         }
 
-        static bool AllLettersGuessed(string maskedWord)
+        private static bool AllLettersGuessed(string maskedWord)
         {
             //determine if the user has guessed all the
             //letters of the word.
             if (maskedWord.Contains("_"))
-
             {
                 return false;
             }
@@ -175,10 +192,10 @@ namespace HangNail
                 return true;
             }
         }
-        ///
-        
-            //One line solution: not maskedWord contains an underscore
-            //return !maskedWord.Contains("_");
-        }
-    }
 
+        ///
+
+        // return invisibility ( the hidden word )
+        
+    }
+}
